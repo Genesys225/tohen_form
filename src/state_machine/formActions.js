7e3ed7a884deef80d65a90a042eac30103c750e1
gene <gene.korn@gmail.com>
2020-01-19 07:59:05 +0200

    checkbox group (obtain), checkbox group, prepare for validation

--- src/state_machine/formActions.js
+++ src/state_machine/formActions.js
@@ -1,111 +1,142 @@
-/* eslint-disable no-fallthrough */
 import { createInputMachine } from "./createInputMachine";
 import { interpret } from "@xstate/fsm";
 import tippy from "tippy.js";
 import { tippyConfig } from "./tippyConf";
 
-function filterFormMembers(node) {
-  switch (node.nodeName) {
-    case "SELECT":
-      node.addEventListener("click", this.handleInput.bind(this));
-    case "INPUT":
-      return true;
-
-    default:
-      break;
-  }
-}
-
-function initializeFormInput(input, inputIndex, htmlInputs) {
-  const { handleFocus, handleInput, handleBlur, formStateService, invalidationDelay, displayMulti } = this;
-  const tippyGlobalConf = { displayMulti };
-	findDuplicates(htmlInputs);
-  this.setState(
-    (state, input, inputIndex) => {
-			input.type === "radio" 
-				? (state[input.name + inputIndex] = input) 
-				: (state[input.name] = input);
-      return state;
-    },
-    input,
-    inputIndex
-  );
-
-  Object.assign(input, {
-    formStateService,
-    required: true,
-    inputIndex,
-    invalidationDelay
-  });
-  // @ts-ignore
-  tippy(input, tippyConfig(tippyGlobalConf));
-  input.addEventListener("input", handleInput.bind(this));
-  input.addEventListener("focus", handleFocus.bind(this));
-  input.addEventListener("blur", handleBlur.bind(this));
-}
 export const actions = {
-  initializeForm: {
-    type: "initializeForm",
-    exec(context, event) {
-      const { tofes } = event;
-      const { shadowRoot, formStateService, form } = tofes;
-      context.formStateService = formStateService;
-      const slot = shadowRoot.querySelectorAll("slot")[0];
-      const nodes = slot.assignedNodes().reverse();
-      /**@type {Array<HTMLInputElement>} - obtain all input elements*/
+	initializeForm: {
+		type: "initializeForm",
+		exec(context, event) {
+			const { tofes } = event;
+			const { shadowRoot, formStateService, form } = tofes;
+			context.formStateService = formStateService;
+			const slot = shadowRoot.querySelector("slot");
+			const nodes = slot.assignedNodes().reverse();
+			/** prepend developer's markup, to the inner form element*/
+			nodes.forEach(node => form.prepend(node));
+			/**@type {Array<HTMLInputElement>} - obtain all input elements*/
 			const htmlInputs = nodes.filter(filterFormMembers, tofes).reverse();
+			createInputGroups(htmlInputs, shadowRoot);
 			/** init the inputs */
 			htmlInputs.forEach(initializeFormInput, tofes);
-			/** prepend developer's to the inner form element*/
-      nodes.forEach(node => form.prepend(node));
-    }
-  },
-  initializeInputs: {
-    type: "initializeInputs",
-    exec(_context, event) {
+		}
+	},
+
+	initializeInputs: {
+		type: "initializeInputs",
+		exec(_context, event) {
 			const { tofes } = event;
 			/** prevent form auto validatio by submit */
-      tofes.form.noValidate = true;
+			tofes.form.noValidate = true;
+			tofes.displayMulti = false;
 			/** init form state machine, and store a reference to it on all inputs */
-      tofes.setState(state => {
-        const inputNames = Object.keys(state);
-        function setState(inputName) {
-          const inputStateMachine = createInputMachine(state[inputName], tofes.displayMulti);
-          state[inputName].inputStateService = interpret(inputStateMachine).start();
-          state[inputName].initialized = true;
-        }
-        inputNames.forEach(setState);
-        return state;
-      });
-    }
-  },
+			tofes.setState(initInputMachines);
+		}
+	},
 
-  announceInitSuccess: {
-    type: "announceInitSuccess",
-    exec(ctx) {
-      console.log(ctx.inputsInitialized && "SUCCESSFULLY INITIALIZED INPUT MACHINES");
-    }
-  }
+	announceInitSuccess: {
+		type: "announceInitSuccess",
+		exec(ctx) {
+			console.log(
+				ctx.inputsInitialized &&
+					"SUCCESSFULLY INITIALIZED INPUT MACHINES"
+			);
+		}
+	}
 };
 
-function findDuplicates(htmlInputs) {
-	// create a sorted inputs names array
-  const inputNames = htmlInputs.map(input => input.name).sort();
-	// create a sorted duplicate names array
-  const duplicates = inputNames.reduce((res, input, i) => {
-		inputNames[i + 1] === input && !res[input] && (res[input] = input);
-    return res;
-	}, {});
-	console.log(duplicates);
-	// create a duplicate name, input groups object, with duplicate name inputs arrays
-	// { groupName: [input, input], group2Name: [input, input, input]}
-  return htmlInputs
-		.filter(function(input) {
-			return Object.values(this).includes(input.name);
-		}, duplicates)
-		/** assign the groups to each member of the group */
-		.map((input, _i, duplicateInputs) => {
-			input[input.name] = duplicateInputs;
-			return input;
-		});
+function filterFormMembers(node) {
+	switch (node.nodeName) {
+		case "SELECT":
+			node.addEventListener("click", this.handleInput.bind(this));
+			return true;
+		case "INPUT":
+			return true;
+
+		default:
+			break;
+	}
+}
+
+function createInputGroups(htmlInputs, shadowRoot) {
+	htmlInputs.forEach(input => {
+		const validName = input.name.replace(/\W/g, "");
+		const group = shadowRoot.querySelectorAll(`[name="${input.name}"]`);
+		group.length > 1 && (input[validName] = group);
+	});
+}
+
+function initializeFormInput(input, inputIndex) {
+	const {
+		handleFocus,
+		handleInput,
+		handleBlur,
+		formStateService,
+		invalidationDelay,
+		displayMulti
+	} = this;
+	const tippyGlobalConf = { displayMulti };
+	this.setState(initInputState, input, inputIndex);
+
+	Object.assign(input, {
+		formStateService,
+		required: true,
+		inputIndex,
+		invalidationDelay
+	});
+	// @ts-ignore
+	tippy(input, tippyConfig(tippyGlobalConf));
+	input.addEventListener("input", handleInput.bind(this));
+	input.addEventListener("focus", handleFocus.bind(this));
+	input.addEventListener("blur", handleBlur.bind(this));
+}
+
+function initInputMachines(state) {
+	const inputNames = Object.keys(state);
+	inputNames.forEach(initMachine, state);
+	function initMachine(inputName) {
+		const state = this;
+		const inputStateMachine = createInputMachine(state[inputName]);
+		state[inputName].inputStateService = interpret(
+			inputStateMachine
+		).start();
+		state[inputName].initialized = true;
+	}
+	return state;
+}
+
+const validationOverrides = {
+	get(target, prop) {
+		if (prop === "checkValidity" && target.type === "checkbox") {
+			const groupValidMember = target.form.querySelector(
+				`[name="${target.name}"]:checked`
+			);
+			const correctTarget =
+				groupValidMember && !target.checked ? groupValidMember : target;
+			return correctTarget[prop].bind(correctTarget);
+		}
+		if (typeof target[prop] === "function")
+			return target[prop].bind(target);
+
+		return target[prop];
+	}
+};
+
+function initInputState(state, input, inputIndex) {
+	switch (input.type) {
+		case "radio":
+			state[input.name + inputIndex] = input;
+			break;
+		case "checkbox":
+			state[input.name + inputIndex] = new Proxy(
+				input,
+				validationOverrides
+			);
+			break;
+
+		default:
+			state[input.name] = input;
+			break;
+	}
+	return state;
 }
